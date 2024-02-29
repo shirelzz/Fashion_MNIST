526d174d2597d8d487f6599d94c791f4e61b8ec7
shirelzz <99799701+shirelzz@users.noreply.github.com>
2024-02-29 20:34:09 +0200

    final CNN with complete documentation

--- fashion_mnist/CNN.py
+++ fashion_mnist/CNN.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
-# from keras.src.layers import LeakyReLU
 
-# from tensorflow.keras import layers, models
 layers, models = tf.keras.layers, tf.keras.models
-BatchNormalization, Dropout, LeakyReLU = tf.keras.layers.BatchNormalization, tf.keras.layers.Dropout, tf.keras.layers.LeakyReLU
+BatchNormalization, Dropout, LeakyReLU, EarlyStopping = tf.keras.layers.BatchNormalization,\
+                                                        tf.keras.layers.Dropout,\
+                                                        tf.keras.layers.LeakyReLU,\
+                                                        tf.keras.callbacks.EarlyStopping
+
 
 def create_cnn_model(input_shape, num_classes):
 
@@ -14,49 +16,19 @@ def create_cnn_model(input_shape, num_classes):
 
     Description:
     This function constructs a CNN model using the Keras Sequential API.
-    The model architecture consists of several layers:
-
-    Convolutional Layers: These layers apply a convolution operation to the input image,
-    extracting various features through filters.
-    The activation function used is ReLU,
-    which introduces non-linearity into the model, enabling it to learn complex patterns.
-
-    Batch Normalization: This technique is used to improve the training stability and speed
-    by normalizing the inputs of each layer.
-
-    MaxPooling Layers: Max pooling reduces the spatial dimensions of the feature maps,
-    effectively downsampling the input.
-    It retains the most important features while reducing computational
-    complexity and preventing overfitting.
-
-    Flatten Layer: This layer flattens the 2D feature maps into a 1D vector,
-    preparing the data for input into a fully connected neural network.
-
-    Dense Layers: Fully connected layers that perform classification based on the learned features.
-    The first dense layer has 64 units with ReLU activation, enabling the network to learn complex
-    patterns in the flattened feature vectors.
-    The final dense layer has units equal to the number of classes
-    in the dataset, with softmax activation, which outputs probabilities for each class,
-    indicating the likelihood of the input image belonging to each class.
-
-    Dropout: This layer was added to prevent overfitting.
-    It works by randomly setting a fraction of input units to zero during training,
-    which helps to prevent the network from relying too much on specific neurons
-    and encourages it to learn more robust features.
-    We've defined a Dropout layer with a dropout rate of 0.2.
-    This means during training, 20% of the input units to the Dropout layer will be randomly set to zero.
+    The model architecture consists of several layers.
+    A more detailed explanation for each layer or step
+    in this function can be found in the article.
 
     Parameters:
     - input_shape: Tuple specifying the shape of the input images (height, width, channels).
     - num_classes: Integer indicating the number of classes in the classification task.
 
-    notes:
-        - I was experiencing with ReLU and Leaky ReLU and the results were slightly better with ReLU.
-        - Deepening the layers more with MaxPooling2D is impossible because the input images are relatively small (28*28)
+    Returns:
+    - model: A model object.
 
     """
-    # activation = 'relu'
-    activation = LeakyReLU(alpha=0.1)
+    activation = 'relu'
     dropout_rate = 0.3
 
     model = models.Sequential([
@@ -79,13 +51,27 @@ def create_cnn_model(input_shape, num_classes):
 
 def train_cnn_model(X_train, y_train, X_test, y_test, num_classes):
     """
-    notes: I was experiencing with different parameters:
-    - number of epochs: 8, 10, 12, 16
-      results were best with 8
-    - optimizers: Adam, SGD, RMSprop
-      results were best with Adam
-    - loss functions:
+    This function prepares the training and test datasets by reshaping them into the format
+    required for CNN input, constructs the CNN model by calling the `create_cnn_model` function,
+    compiles the model and trains the model using the provided training data.
+    I used early stopping to halt training if the validation loss does not improve for three consecutive epochs,
+    trying to avoid overfitting.
+    The training process's history is plotted, showing metrics evolution over epochs.
 
+    Parameters:
+    - X_train: training data features.
+    - y_train: training data labels.
+    - X_test: test data features.
+    - y_test: test data labels.
+    - num_classes: number of classes in the dataset.
+
+    Returns:
+    - model: The trained model object.
+
+    The model expects input data in the shape of 28x28 pixel grayscale images and is tailored
+    for a classification task with a specified number of classes. After training, the function
+    plots the training and validation accuracy and loss metrics over epochs and returns the
+    trained model for further use or evaluation.
     """
     # Reshape the input data for CNN
     X_train = np.array(X_train).reshape(-1, 28, 28, 1)
@@ -95,19 +81,26 @@ def train_cnn_model(X_train, y_train, X_test, y_test, num_classes):
     model = create_cnn_model((28, 28, 1), num_classes)  # grayscale image of size 28x28 pixels with one channel.
 
     # Compile the model
-    # optimizer = tf.keras.optimizers.legacy.RMSprop(learning_rate=0.001, momentum=0.9)
-    optimizer = 'adam'
-    model.compile(optimizer=optimizer,
+    model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
     # Train the model
-    epochs = 8
-    model_history = model.fit(X_train, y_train, epochs=epochs, batch_size=32, validation_data=(X_test, y_test))
+    epochs = 20
+    model_history = model.fit(X_train, y_train,
+                              epochs=epochs,
+                              batch_size=32,
+                              validation_data=(X_test, y_test),
+                              callbacks=[early_stopping])
+
+    # Get the epoch where the fit stopped in
+    stop = early_stopping.stopped_epoch
 
     # Display the training history
     pd.DataFrame(model_history.history).plot(figsize=(8, 5))
-    plt.title('CNN Training History, epochs: ' + str(epochs))
+    plt.title('CNN Training History \n epoch: ' + str(stop))
     plt.show()
 
     return model
